649338d0525a8b359e081071ca9e733180f1eaed
eagleae7 <spotarinz@gmail.com>
2022-11-24 20:49:16 +0100

    printf

--- functions2.c
+++ functions2.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+/****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
@@ -51,6 +52,7 @@ int print_pointer(va_list types, char buffer[],
 		width, flags, padd, extra_c, padd_start));
 }
 
+/************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
  * @types: Lista of arguments
